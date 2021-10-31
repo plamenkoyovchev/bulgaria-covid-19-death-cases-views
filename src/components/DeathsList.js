02de16c4c23ab24e20b76409bedd9bddfb5384f4
plamenkoyovchev <plamenkooo@gmail.com>
2021-10-31 10:18:00 +0200

    Update age ranges

--- src/components/DeathsList.js
+++ src/components/DeathsList.js
@@ -5,7 +5,7 @@ import { data } from '../data';
 import { findDeathsCountByAgeRange } from "../utils/functions";
 import DeathListItem from './DeathListItem';
 
-const ageRanges = ["0 - 9", "10 - 19", "20 - 29", "30 - 39", "40 - 49", "50 - 59", "60 - 69", "70 - 79", "80 - 89", "90 - 99", "100 - 109"];
+const ageRanges = ["0 - 9", "10 - 19", "20 - 29", "30 - 39", "40 - 49", "50 - 59", "60 - 69", "70 - 79", "80 - 89", "90+"];
 
 const DeathsList = () => {
     const [info, setInfo] = useState({
