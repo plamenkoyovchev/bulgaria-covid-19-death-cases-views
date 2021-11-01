d8364ee06f4603f2f82c60823a3b42b127c66870
plamenkoyovchev <plamenkooo@gmail.com>
2021-11-01 10:39:28 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import DeathsList from './components/DeathList/DeathList';
+import VaccinatedDeathList from './components/VaccinatedDeathList/VaccinatedDeathList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <VaccinatedDeathList />
         <DeathsList />
         <div className="App-small-text">* Данните са взети от <a href="https://data.egov.bg/data/resourceView/18851aca-4c9d-410d-8211-0b725a70bcfd" target="_blank" rel="noreferrer">Портал за отворени данни</a> и е възможно да има неточности!</div>
         <div className="App-small-text">* Сайтът е разработен само с информативна цел!</div>
