3dd3fa7e1d03f3eb362f86e9efd43cf989673aad
plamenkoyovchev <plamenkooo@gmail.com>
2021-11-01 11:18:28 +0200

    Add vaccinated table from coronavirus portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import DeathsList from './components/DeathList/DeathList';
 import VaccinatedDeathList from './components/VaccinatedDeathList/VaccinatedDeathList';
+import vaccinated from "./vaccinated.PNG";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <img width={830} height={90} src={vaccinated} alt="Vaccinanted" />
+        </div>
         <VaccinatedDeathList />
         <DeathsList />
         <div className="App-small-text">* Данните са взети от <a href="https://data.egov.bg/data/resourceView/18851aca-4c9d-410d-8211-0b725a70bcfd" target="_blank" rel="noreferrer">Портал за отворени данни</a> и е възможно да има неточности!</div>
