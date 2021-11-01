8fe185d0d936a62dcb2d2b8ed75f826c18a8899f
plamenkoyovchev <plamenkooo@gmail.com>
2021-11-01 13:48:24 +0200

    Put all components in Accordion

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import './App.css';
 import CovidSpreadTableByAge from './components/CovidSpreadTableByAge/CovidSpreadTableByAge';
 import DeathsList from './components/DeathList/DeathList';
 import VaccinatedDeathList from './components/VaccinatedDeathList/VaccinatedDeathList';
 import vaccinated from "./vaccinated.PNG";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CovidSpreadTableByAge />
-        <div>
-          <img width={830} height={90} src={vaccinated} alt="Vaccinanted" />
+        <div className="Accordion-container">
+          <Accordion expanded={true}>
+            <AccordionSummary
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+              expandIcon={<ExpandMoreIcon />}
+            >
+              <Typography>Разпространение на COVID-19 по възрастови групи</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <CovidSpreadTableByAge />
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+              expandIcon={<ExpandMoreIcon />}
+            >
+              <Typography>Ваксинирани срещу COVID-19 по възрастови групи</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div>
+                <img width={830} height={90} src={vaccinated} alt="Vaccinanted" />
+              </div>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={true}>
+            <AccordionSummary
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+              expandIcon={<ExpandMoreIcon />}
+            >
+              <Typography>Ваксинирани починали от COVID-19 по възрастови групи</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <VaccinatedDeathList />
+            </AccordionDetails>
+          </Accordion>
+          <Accordion expanded={true}>
+            <AccordionSummary
+              aria-controls="panel4a-content"
+              id="panel4a-header"
+              expandIcon={<ExpandMoreIcon />}
+            >
+              <Typography>Починали от COVID-19 по възрастови групи</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <DeathsList />
+            </AccordionDetails>
+          </Accordion>
+          <div className="App-small-text">* Данните са взети от <a href="https://data.egov.bg/data/resourceView/18851aca-4c9d-410d-8211-0b725a70bcfd" target="_blank" rel="noreferrer">Портал за отворени данни</a> и е възможно да има неточности!</div>
+          <div className="App-small-text">* Сайтът е разработен само с информативна цел!</div>
         </div>
-        <VaccinatedDeathList />
-        <DeathsList />
-        <div className="App-small-text">* Данните са взети от <a href="https://data.egov.bg/data/resourceView/18851aca-4c9d-410d-8211-0b725a70bcfd" target="_blank" rel="noreferrer">Портал за отворени данни</a> и е възможно да има неточности!</div>
-        <div className="App-small-text">* Сайтът е разработен само с информативна цел!</div>
       </header>
     </div>
   );
