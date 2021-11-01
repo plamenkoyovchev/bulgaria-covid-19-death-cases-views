776efefde36e8249a535491aac809769282ccac3
plamenkoyovchev <plamenkooo@gmail.com>
2021-11-01 10:38:26 +0200

    Add function to group by vaccine

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,4 @@
+// All
 // Data format
 // [
 //     "Дата",
@@ -10,7 +11,7 @@ const ageRangeIndex = 2;
 const deathsCountIndex = 3;
 
 export const findDeathsCountByAgeRange = (ageRange, data) => {
-    const filteredData = filterDataByAgeRange(ageRange, data);
+    const filteredData = data.filter(rec => rec[ageRangeIndex] === ageRange);
     const result = {
         label: ageRange,
         count: filteredData.reduce((prevCount, currentRecord) => prevCount + (+currentRecord[deathsCountIndex]), 0)
@@ -19,6 +20,43 @@ export const findDeathsCountByAgeRange = (ageRange, data) => {
     return result;
 };
 
-const filterDataByAgeRange = (ageRange, data) => {
-    return data.filter(rec => rec[ageRangeIndex] === ageRange);
+export const getKeys = (data) => {
+    const map = {};
+
+    for (const record of data) {
+        if (!map[record[2]]) {
+            map[record[2]] = true;
+        }
+    }
+
+    return map;
+};
+
+// Only vaccinated
+// ["Дата", "Ваксина", "Пол", "Възрастова група", "Брой починали"]
+
+const vaccineIndex = 1;
+const vaccinatedAgeRangeIndex = 3;
+const vaccinatedDeathsCountIndex = 4;
+
+export const groupDeathsByVaccine = (ageRange, data) => {
+    const filteredData = data.filter(rec => rec[vaccinatedAgeRangeIndex] === ageRange);
+    const map = {
+        total: 0
+    };
+
+    for (const record of filteredData) {
+        const vaccine = record[vaccineIndex];
+        if (!map[vaccine]) {
+            map[vaccine] = {
+                count: +record[vaccinatedDeathsCountIndex]
+            };
+        } else {
+            map[vaccine].count += +record[vaccinatedDeathsCountIndex];
+        }
+
+        map.total += +record[vaccinatedDeathsCountIndex];
+    }
+
+    return map;
 };
